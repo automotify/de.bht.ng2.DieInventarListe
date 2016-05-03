660b4bf819880295a3f1b1f6d51ec5b90b340d9e
András Bucsi <bucsi@beuth-hochschule.de>
2016-05-03 23:23:07 +0200

    changes to hero.ts (code formatting)

--- app/hero.ts
+++ app/hero.ts
@@ -4,17 +4,16 @@
  * @author András Bucsi
  */
 export class Hero {
-    private name  :string;
-    private age   :number;
-    private health:number;
-
+    private name   :string;
+    private age    :number;
+    private health :number;
 
     /**
      * Constructor
      * @param name
      * @param age
      */
-    constructor(name: string, age: number) {
+    constructor(id: number, name: string, age: number) {
         this.name   = name;
         this.age    = age;
         this.health = 100; // Health is at 100 (%) upon initialisation
