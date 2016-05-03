03de6088330a5b4335a507b2da4ac92bcbb03861
András Bucsi <bucsi@beuth-hochschule.de>
2016-05-03 17:58:10 +0200

    Hero Model

---
+++ app/hero.ts
@@ -0,0 +1,22 @@
+/**
+ * Hero representation
+ *
+ * @author András Bucsi
+ */
+export class Hero {
+    private name  :string;
+    private age   :number;
+    private health:number;
+
+
+    /**
+     * Constructor
+     * @param name
+     * @param age
+     */
+    constructor(name: string, age: number) {
+        this.name   = name;
+        this.age    = age;
+        this.health = 100; // Health is at 100 (%) upon initialisation
+    }
+}
