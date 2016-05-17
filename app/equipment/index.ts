bc12e1eb8753d0256ef0ed57545505bc7f76dfe8
automotify <automotify@users.noreply.github.com>
2016-05-17 17:04:44 +0200

    Delete index.ts

--- app/equipment/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Barrel for package equipment
- */
-import {Equipment} from './equipment';
-import {EquipmentComponent} from './equipment.component'
-
-export {Equipment} from './equipment';
-export {EquipmentComponent} from './equipment.component';
