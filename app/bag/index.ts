abe3fd87ab842a85ee30d1760715cd98f31dd149
lPhenax <schleussi1990@gmail.com>
2016-05-17 17:04:19 +0200

    Delete index.ts

--- app/bag/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Barrel for package bag
- */
-import {Bag} from './bag';
-import {BagComponent} from './bag.component'
-
-export {Bag} from './bag';
-export {BagComponent} from './bag.component';
