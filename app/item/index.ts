077cee33b83f03e32d4b5c6d5b18c0231f2823d9
lPhenax <schleussi1990@gmail.com>
2016-05-17 17:05:01 +0200

    Delete index.ts

--- app/item/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Barrel for package item
- */
-import {Item} from './item';
-import {ItemDetailComponent} from './item-detail.component';
-
-export {Item} from './item'
-export {ItemDetailComponent} from './item-detail.component'
