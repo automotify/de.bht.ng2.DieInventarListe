c26a6174677162c9f18464ae0e6aba2e1d7d49c9
automotify <automotify@users.noreply.github.com>
2016-05-21 23:56:47 +0200

    Update index.ts

--- app/hero/index.ts
+++ app/hero/index.ts
@@ -1,16 +1,6 @@
 /**
  * Barrel for package equipment
  */
-<<<<<<< Updated upstream
-import {Hero} from './hero.model';
-import {HeroList} from './hero-list.model';
-import {HeroListComponent} from './hero-list.component';
-import {HeroDetailComponent} from './hero-detail.component';
-
-export {Hero} from './hero.model';
-<<<<<<< HEAD
-export {HeroComponent} from './hero.component';
-=======
 import {Hero}                from './hero.model';
 import {HeroList}            from './hero-list.model';
 import {HeroListComponent}   from './hero-list.component';
@@ -19,9 +9,4 @@ import {HeroDetailComponent} from './hero-detail.component';
 export {Hero}                from './hero.model';
 export {HeroList}            from './hero-list.model';
 export {HeroListComponent}   from './hero-list.component';
->>>>>>> Stashed changes
-=======
-export {HeroList} from './hero-list.model';
-export {HeroListComponent} from './hero-list.component';
->>>>>>> de7fff6b259fc4dbaa43d7dd50eabfbbcf1c161d
 export {HeroDetailComponent} from './hero-detail.component';
